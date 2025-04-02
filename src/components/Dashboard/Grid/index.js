a2695496c7febe383d2ed043cd9bc06d71ac9e9c
cry-wizard <sidd160306@gmail.com>
2025-04-02 07:59:18 +0530

    Grid_Complete

--- src/components/Dashboard/Grid/index.js
+++ src/components/Dashboard/Grid/index.js
@@ -5,7 +5,8 @@ import TrendingDownRoundedIcon from '@mui/icons-material/TrendingDownRounded';
 
 const Grid = ({ coin }) => {
   return (
-    <div className="grid-container">
+    <div className={`grid-container ${coin.price_change_percentage_24h < 0 && "grid-cointiner-red" 
+    }`}>
       <div className="info-flex">
         <img src={coin.image} className="coin-logo" alt="coin-logo" />
         <div className="name-col">
@@ -28,6 +29,12 @@ const Grid = ({ coin }) => {
           <div className="icon-chip chip-red"><TrendingDownRoundedIcon/></div>
         </div>
       )}
+      <div className="info-container">
+      <h3 className="coin-price" style={{color:coin.price_change_percentage_24h < 0 ?"var(--red)":"var(--green)" }}>${coin.current_price.toLocaleString()}
+      </h3>
+      <p className="total_volume">Total Volume : {coin.total_volume.toLocaleString()}</p>
+      <p className="total_volume">Market Cap : {coin.market_cap.toLocaleString()}</p>
+      </div>
     </div>
   );
 };
